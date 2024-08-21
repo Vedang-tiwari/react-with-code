3dc6cfbe1a7883513c650b793278dcb37b8b039e
vedang tiwari <vedangt027@gmail.com>
2024-08-21 16:23:10 +0530

    learn about props in react and tailwind

---
+++ 2tailwindprops/src/App.jsx
@@ -0,0 +1,14 @@
+
+import Card from "./components/card"
+function App() {
+  return (
+    <>
+    <h1>tailwind</h1><br/>
+    <Card username = "vedang" btnText="click me"/>
+    <Card username = "react" btnText="visit me"/>
+    
+    </>
+  )
+}
+
+export default App
